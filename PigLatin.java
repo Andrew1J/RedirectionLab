04d18d3e2a17f56db06e9e863d6803f3f78555fe
Andrew <ajuang20@stuy.edu>
2021-01-04 14:49:26 -0500

    Added second method

--- PigLatin.java
+++ PigLatin.java
@@ -14,4 +14,22 @@ public class PigLatin {
 			return s.substring(1) + s.charAt(0) + "ay";
 		}
     }
+
+    public static String pigLatin(String s){
+        s = s.toLowerCase();
+        String[]diagraphs =  {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh",
+        "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp",
+        "st", "sw", "th", "tr", "tw", "wh", "wr"};
+        boolean isDiagraph = false;
+        for(int i=0;i<diagraphs.length;i++){
+            if (s.substring(0,2).equals(diagraphs[i]){
+                isDiagraph = true;
+            }
+        }
+        if(isDiagraph){
+            return s.substring(2) + s.substring(0,2) + "ay";
+        } else {
+            return pigLatinSimple(s);
+        }
+    }
 }
