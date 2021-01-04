24ec4423e962f2f9034c2f40cccff4f69d3da823
Andrew <ajuang20@stuy.edu>
2021-01-04 13:02:12 -0500

    Added and tested MakeStars

--- MakeStars.java
+++ MakeStars.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class MakeStars {
     public static void main(String[] args){
-        
+        Scanner s = new Scanner(System.in);
+        while(s.hasNextLine()){
+            Scanner line = new Scanner(s.nextLine());
+            while(line.hasNext()){
+                String word = line.next();
+                for(int i=0;i<word.length();i++){
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
