9a4eae0a9b078305af46780165518799bdc93bca
Andrew <ajuang20@stuy.edu>
2021-01-04 15:10:54 -0500

    Debugged third method

--- PigLatin.java
+++ PigLatin.java
@@ -2,13 +2,31 @@ import java.util.Scanner;
 
 public class PigLatin {
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        while(s.hasNext()){
-            Scanner words = new Scanner(s);
-            String res = "";
-            
-            String line = s.nextLine()
-        }
+        System.out.println(pigLatin("the"));
+        System.out.println(pigLatin("check"));
+        System.out.println(pigLatin("skee"));
+        System.out.println(pigLatin("emu"));
+        System.out.println(pigLatin("grade"));
+
+        System.out.println(pigLatinBest("*emu"));
+        System.out.println(pigLatinBest("4chan"));
+        System.out.println(pigLatinBest("fish!"));
+        System.out.println(pigLatinBest("fish"));
+        System.out.println(pigLatinBest("the."));
+        System.out.println(pigLatinBest("cat!"));
+        System.out.println(pigLatinBest("amazing?"));
+        System.out.println(pigLatinBest("apple%"));
+        /*Scanner s = new Scanner(System.in);
+        while(s.hasNextLine()){
+            Scanner words = new Scanner(s.nextLine());
+            while(words.hasNext()){
+                System.out.print(pigLatinBest(words.next()));
+                if(words.hasNext()){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }*/
     }
 
     public static String pigLatinSimple(String s){
@@ -29,7 +47,7 @@ public class PigLatin {
         "st", "sw", "th", "tr", "tw", "wh", "wr"};
         if(s.length()>=2){
             for(int i=0;i<diagraphs.length;i++){
-                if (s.substring(0,2).equals(diagraphs[i]){
+                if (s.substring(0,2).equals(diagraphs[i])){
                     isDiagraph = true;
                 }
             }
@@ -46,11 +64,13 @@ public class PigLatin {
         if(!Character.isLetter(s.charAt(0))){
             return s;
         }
-        String ans = "";
+        String res = "";
         if(!Character.isLetter(s.charAt(s.length()-1))){
             res = pigLatin(s.substring(0,s.length()-1));
+        } else {
+            res = pigLatin(s);
         }
-        if(!Character.isDigit(s.charAt(s.length()-1))){
+        if(!Character.isDigit(s.charAt(s.length()-1))&&!Character.isLetter(s.charAt(s.length()-1))){
             res += s.charAt(s.length()-1);
         }
         return res;
