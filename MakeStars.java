2ad38ccd9b5da35b28ad30d1d99759d7a0c4964a
Andrew <ajuang20@stuy.edu>
2021-01-04 12:47:08 -0500

    Initial Commit

---
+++ MakeStars.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class MakeStars {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ PigLatin.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class PigLatin {
+    public static void main(String[] args){
+
+    }
+}
