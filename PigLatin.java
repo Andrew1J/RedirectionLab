60c1f0388cc3a9dafc52c65080701d97a4e06915
Andrew <ajuang20@stuy.edu>
2021-01-04 13:08:19 -0500

    Added first method

--- PigLatin.java
+++ PigLatin.java
@@ -4,4 +4,14 @@ public class PigLatin {
     public static void main(String[] args){
 
     }
+
+    public static String pigLatinSimple(String s){
+        s = s.toLowerCase();
+        if(s.charAt(0)== 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o'
+        || s.charAt(0) == 'u'){
+			return s + "hay";
+		} else {
+			return s.substring(1) + s.charAt(0) + "ay";
+		}
+    }
 }
